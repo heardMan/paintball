17bf0ff1f04b183ba342fb2b783d4a79258be17e
Rachel Bautista <rachelbaut5@gmail.com>
2019-04-23 18:44:12 -0700

    images, navbar

--- client/src/Components/Navbar/Navbar.js
+++ client/src/Components/Navbar/Navbar.js
@@ -1,44 +1,43 @@
 import React, { Component } from "react";
 
+import "./Navbar.css";
+
+
 class Navbar extends Component {
     loggedIn = () => {
         if (this.props.state.roles.length < 1) {
             return (
-                <ul className="navbar-nav ml-auto">
-                    <li className="nav-item active">
-                        <a className="nav-link" href="/signIn">Sign In </a>
-                    </li>
-                    <li className="nav-item active">
-                        <a className="nav-link" href="/register">Register</a>
-                    </li>
-                </ul>
+                <nav className="navigation__nav">
+                    <ul className="navigation__list">
+                        <li className="navigation__item"><a href="/signin" className="navigation__link"><span>01</span> Sign In</a></li>
+                        <li className="navigation__item"><a href="/register" className="navigation__link"><span>02</span> Register</a></li>
+                    </ul>
+                </nav>
             );
 
         } else {
             return (
-                <ul className="navbar-nav ml-auto">
-                    {this.props.state.roles.map((role, i) => {
-                        let roleName = role === "manager" ? "Manager View" : "Tenant View";
-                        return (
-                            <li key={i} className="nav-item active">
-                                <a className="nav-link" href={`/${role}`}>{roleName}</a>
-                            </li>
-                        )
-                    }
-                    )}
-                    <li>
-                        <a className="nav-link" onClick={this.props.signOut} href="/signOut">Sign Out</a>
-                    </li>
-                </ul>
+                <nav className="navigation__nav">
+                    <ul className="navigation__list">
+                        <li className="navigation__item"><a href="/signout" className="navigation__link"><span>01</span> Sign Out</a></li> 
+                    </ul>
+                </nav>
             )
         }
     }
+        
     render() {
         return (
-            <nav className="navbar navbar-expand navbar-light bg-light fixed-top">
-                <a className="navbar-brand" href="/">GTP</a>
+            <div className="navigation">
+                 <input type="checkbox" className="navigation__checkbox" id="navi-toggle"/>
+
+                    <label for="navi-toggle" className="navigation__button">
+                        <span className="navigation__icon">&nbsp;</span>
+                    </label>
+
+                <div className="navigation__background">&nbsp;</div>
                 {this.loggedIn()}
-            </nav>
+            </div>
         );
     }
 }
