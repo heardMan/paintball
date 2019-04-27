1ea403e1ad8c1a54905e8f7d3bcf0cd50810c128
jcasas5253 <skedabble@gmail.com>
2019-04-27 06:55:28 -0700

    3

--- server.js
+++ server.js
@@ -15,6 +15,9 @@ app.use(express.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
 }
 // Import routes
 app.use(routes);
